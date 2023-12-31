5b7d35e0767c35b2c8d9e9a069e690142ab4857f
Conrad Hansen-Quartey <Chansenq@gmail.com>
2023-12-31 15:47:48 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,11 @@
 import { render } from "react-dom";
 
-import App from "./App";
-import Test from "./Test";
+import App from "./App"; 
 // import Sample from "./PieChart2"; // deleted
 import ScanProcessingJS from "./ScanProcessingJS";
-import DoubleSpaceEnforcer from "./DoubleSpaceEnforcer";
-import GlassDoorReviewFormatter from "./GlassDoorReviewFormatter";
-import SentConnections from "./SentConnectionsLessBuggy";
-
-// SentConnections
-
+ 
 const rootElement = document.getElementById("root");
 render(<ScanProcessingJS />, rootElement);
-// render(<SentConnections />, rootElement);
-// render(<DoubleSpaceEnforcer />, rootElement);
 
-// render(<GlassDoorReviewFormatter />, rootElement);
-
-// render(<Test />, rootElement);
 // render(<App />, rootElement);
 // render(<Sample />, rootElement); // deleted
